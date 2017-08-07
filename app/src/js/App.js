d43d1c749a631bf3c21c2245c9a20aed0435f6f4
Rufo Gabrillo <rufo.gabrillo@gmail.com>
2017-08-07 22:13:27 +0800

    update

---
+++ app/src/js/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from '.././logo.svg';
+import f7_css from '../css/framework7/framework7.material.min.css';
+import f7color_css from '../css/framework7/framework7.material.colors.min.css';
+import f73d_css from '../css/framework7/framework7.3dpanels.min.css';
+import f7icons_css from '../css/framework7/framework7-icons.css';
+// import f7_css from '';
+// import f7_css from '';
+// import './App.css';
+
+class App extends Component{
+
+  render(){
+    return(
+      <h1>
+        hello world
+      </h1>
+    );
+  }
+}
+export default App;
